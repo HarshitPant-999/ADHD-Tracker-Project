279f68d2bbbcb032917a6e48caf764d8b9fdeebf
Bananabob-hp3 <pantharahit97@gmail.com>
2026-08-01 15:18:04 +0530

    Adding log-entry route

--- routes/entries.py
+++ routes/entries.py
@@ -4,5 +4,13 @@ entries_bp = Blueprint("entries", __name__)
 
 @entries_bp.route("/log", methods=["GET", "POST"])
 def log_entry():
-    ...
-
+    form = EntryForm()
+    if form.validate_on_submit():
+        user_data = Entry(
+            time = form.timestamp.data,
+            trigger = form.trigger.data,
+            reset_info = form.reset_info.data,
+            reset_time = form.reset_time.data)
+        db.session.add(user_data)
+        db.session.commit()
+    return render_template("log_entry.html", form=form)
