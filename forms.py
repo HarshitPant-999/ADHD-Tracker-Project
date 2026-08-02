058147a691b4ac9f1fcd8b5fe97cba321e2a4b97
Bananabob-hp3 <pantharahit97@gmail.com>
2026-08-02 20:20:25 +0530

    Adding dashboard route and dashboard.html

--- forms.py
+++ forms.py
@@ -1,12 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, IntegerField, DateTimeLocalField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, URL
 
 class EntryForm(FlaskForm):
     timestamp = DateTimeLocalField("When did this happen?", format="%Y-%m-%dT%H:%M", validators=[DataRequired()])
     trigger = TextAreaField("What do you think caused the crash", validators=[DataRequired()])
-    reset_info = TextAreaField("How did it end?", validators=[DataRequired()])
-    reset_time = IntegerField("How long it took recover?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 class ResolveForm(FlaskForm):
--- routes/entries.py
+++ routes/entries.py
@@ -1,4 +1,13 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for
+from forms import EntryForm, ResolveForm
+from models import Entry, db
+from datetime import datetime, timedelta
+
+
+today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+today_start = today
+tomorrow_start = today + timedelta(days=1)
 
 entries_bp = Blueprint("entries", __name__)
 
@@ -7,10 +16,31 @@ def log_entry():
     form = EntryForm()
     if form.validate_on_submit():
         user_data = Entry(
-            time = form.timestamp.data,
-            trigger = form.trigger.data,
-            reset_info = form.reset_info.data,
-            reset_time = form.reset_time.data)
+            timestamp = form.timestamp.data,
+            trigger = form.trigger.data)
+        print(form.timestamp.data)
         db.session.add(user_data)
         db.session.commit()
+        return redirect(url_for("entries.resolve", entry_id=user_data.id))
     return render_template("log_entry.html", form=form)
+
+@entries_bp.route("/resolve/<int:entry_id>", methods=["GET", "POST"])
+def resolve(entry_id):
+    form = ResolveForm()
+    log_to_update = db.get_or_404(Entry, entry_id)
+    if form.validate_on_submit():
+        log_to_update.reset_info = form.reset_info.data
+        log_to_update.reset_time = form.reset_time.data
+        db.session.commit()
+        return redirect(url_for("entries.log_entry"))
+    return render_template("resolve.html", form=form)
+
+@entries_bp.route("/dashboard")
+def dashboard():
+    crashes_today = db.session.execute(
+    db.select(db.func.count()).select_from(Entry).where(
+        Entry.timestamp >= today_start,
+        Entry.timestamp < tomorrow_start
+        )).scalar()
+    return render_template("history.html", crashes_today=crashes_today)
+
